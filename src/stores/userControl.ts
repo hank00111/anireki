4bcd2def0f0b91306c7390343569948b07feb94b
hank00111 <42414986+hank00111@users.noreply.github.com>
2023-09-02 20:16:45 +0800

    Update userControl.ts

--- src/stores/userControl.ts
+++ src/stores/userControl.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { defineStore } from "pinia";
-axios.defaults.baseURL = "https://a2.anireki.com/v2/";
+axios.defaults.baseURL = "https://a2.anireki.com";
 axios.defaults.withCredentials = true;
 //a1.anireki.com/v2/
 
@@ -14,7 +14,7 @@ export const useUserControl = defineStore("login", {
   actions: {
     async testPost() {
       try {
-        let res = await axios.get("https://a2.anireki.com/");
+        let res = await axios.get("/");
         console.log(res);
         // let reas = await axios.post("test", { withCredentials: true });
         // console.log(reas);
