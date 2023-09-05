222fd7770d093b8632d0d8e0d30bac76f0ef76d9
hank00111 <42414986+hank00111@users.noreply.github.com>
2023-09-05 19:11:13 +0800

    Update animeWorks.ts

--- src/stores/animeWorks.ts
+++ src/stores/animeWorks.ts
@@ -5,14 +5,22 @@ axios.defaults.withCredentials = true;
 
 export const useAnimeWorks = defineStore("animeWorks", {
   state: () => ({
-    animeData: [],
+    animeData: [
+      {
+        id: "",
+        title: "",
+        title_jp: "",
+        season: "",
+        images_url: "",
+      },
+    ],
     // drakState: false,
   }),
   actions: {
     async getCurrentSeason() {
       try {
         let res = await axios.get("/works/season/2023-summer");
-        this.animeData = res.data;
+        // this.animeData = res.data;
         console.log(res.data);
       } catch (error) {
         console.log(error);
