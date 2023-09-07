482ac8775f0a469213006bd41006cdd992df2df9
hank00111 <42414986+hank00111@users.noreply.github.com>
2023-09-07 13:39:09 +0800

    v0.1.3.7 b-3

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,12 +6,12 @@ import { useAnimeWorks } from '../stores/animeWorks'
 
 const animeWorks = useAnimeWorks();
 // current season
-onMounted(async () => {
+onMounted(() => {
     document.title = 'Home - Anireki';
     // animeWorks.getCurrentSeason();
 })
 onBeforeMount(async () => {
-    animeWorks.getCurrentSeason();
+    await animeWorks.getCurrentSeason();
 })
 </script>
 
@@ -23,22 +23,21 @@ onBeforeMount(async () => {
         <div class="content">
             <h1>2023 夏季新番</h1>
             <div class="home-info">
-                <div v-for="data in    animeWorks.animeData   " class="works">
+                <!-- <div v-for="data in animeWorks.animeData" class="works">
                     <div class="works-img">
                         <img v-lazy="data.images_url" :alt="data.title_jp">
                     </div>
                     <div>
                         <p class="works-info">{{ data.title }}</p>
                     </div>
-                </div>
-                <!-- <div class="works">
-                    <div class="works-img">
-                        <img src="https://p2.anireki.com/1.jpg" alt="data.title_jp">
+                </div> -->
+                <div v-for="data in animeWorks.animeData" class="works-card">
+                    <div class="images" style="background-image: url({{data.images_url}});">
                     </div>
                     <div>
-                        <p class="works-info">堀與宮村 -piece-</p>
+                        <p class="works-info">{{ data.title }}</p>
                     </div>
-                </div> -->
+                </div>
             </div>
         </div>
     </div>
@@ -88,6 +87,24 @@ onBeforeMount(async () => {
             transition-duration: 400ms;
             transform: scale(1.05);
             filter: drop-shadow(0 0 0.75rem black);
+        }
+    }
+
+    .works-card {
+        display: inline-block;
+
+        .images {
+            height: 0;
+            overflow: hidden;
+            padding-bottom: 141.4%;
+            background-size: cover;
+            background-position: center;
+            border-radius: 10px;
+            position: relative;
+            -webkit-transition: all 0.3s cubic-bezier(0, 0, 0.2, 1);
+            -o-transition: all 0.3s cubic-bezier(0, 0, 0.2, 1);
+            transition: all 0.3s cubic-bezier(0, 0, 0.2, 1);
+            background-color: #eee;
         }
     }
 }
