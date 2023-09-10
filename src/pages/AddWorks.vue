6ecaefc9748ab24ce91beb62ef416cdd42f81cb8
hank00111 <42414986+hank00111@users.noreply.github.com>
2023-09-10 21:19:57 +0800

    v0.1.3.7 b-45

--- src/pages/AddWorks.vue
+++ src/pages/AddWorks.vue
@@ -4,10 +4,10 @@ import Loading from '../components/Loading.vue'
 import ConsoleSidebar from '../components/ConsoleSidebar.vue'
 import { ref, reactive, onMounted, onBeforeMount, watchEffect, watch } from 'vue';
 import { useAnimeWorks } from '../stores/animeWorks'
-// import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router';
 
 const animeWorks = useAnimeWorks();
-// const router = useRouter();
+const router = useRouter();
 
 interface seasonModel {
     [key: number]: any;
@@ -49,8 +49,9 @@ const sendData = () => {
             image: refImages.value
         }
         animeWorks.addWorks(data).then(() => {
-            console.log(animeWorks.sendCode)
-            // router.push('/console')
+            if (animeWorks.sendCode === 0) {
+                router.push('/console')
+            }
         });
 
     } else {
