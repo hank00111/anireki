8a3fbd6852a2b3690e65f9d017f714947659bd88
hank00111 <42414986+hank00111@users.noreply.github.com>
2023-09-22 20:13:07 +0800

    v0.1.3.9 b-27

--- src/stores/userControl.ts
+++ src/stores/userControl.ts
@@ -12,18 +12,17 @@ export const useUserControl = defineStore("login", {
     picture: "",
   }),
   actions: {
-    async getUsera() {
+    async getUser(src: number) {
       try {
         if (this.name.length === 0) {
-          // src: number
-          // let res = await axios.get("/auth/user");
-          // if (res.status === 200) {
-          //   this.isLogin = true;
-          //   this.name = res.data.name;
-          //   this.picture = res.data.picture;
-          // } else if (res.status === 204 && src === 1) {
-          //   window.location.href = "https://a2.anireki.com/v2/auth/google";
-          // }
+          let res = await axios.get("/auth/user");
+          if (res.status === 200) {
+            this.isLogin = true;
+            this.name = res.data.name;
+            this.picture = res.data.picture;
+          } else if (res.status === 204 && src === 1) {
+            window.location.href = "https://a2.anireki.com/v2/auth/google";
+          }
         }
       } catch (error) {
         // console.log(error);
