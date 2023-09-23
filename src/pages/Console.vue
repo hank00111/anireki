57736cf49d85a9da6fbf7e746dfe18b6608e5e5e
hank00111 <42414986+hank00111@users.noreply.github.com>
2023-09-23 15:05:57 +0800

    v0.1.4.0 b-12

--- src/pages/Console.vue
+++ src/pages/Console.vue
@@ -91,7 +91,8 @@ onBeforeUnmount(() => {
     display: flex;
     width: 100%;
     height: 100%;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
     .log-card {
         color: #fff;
