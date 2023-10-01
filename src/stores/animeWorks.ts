09bb4acb233ab2cb1c5b81287074bc53377972bb
hank00111 <42414986+hank00111@users.noreply.github.com>
2023-10-01 19:15:58 +0800

    v0.1.5.0 b-22

--- src/stores/animeWorks.ts
+++ src/stores/animeWorks.ts
@@ -95,9 +95,10 @@ export const useAnimeWorks = defineStore("animeWorks", {
         let res = await axios.get("/works/all");
         const data = JSON.parse(LZString.decompressFromUTF16(res.data));
         data.sort((a: originDataModel, b: originDataModel) => {
-          if (!a.StartedAt_jp) return 1;
-          if (a.StartedAt_jp === b.StartedAt_jp) return 0;
-          return a.StartedAt_jp > b.StartedAt_jp ? 1 : -1;
+          console.log(a.id + " " + ((!a.StartedAt_jp ? 1 : 0) - (!b.StartedAt_jp ? 1 : 0)));
+          // if (a.StartedAt_jp === b.StartedAt_jp) return 0;
+          // if (!a.StartedAt_jp) return 1;
+          // return a.StartedAt_jp > b.StartedAt_jp ? 1 : -1;
         })
         // this.animeData = data;
         this.originData = data;
