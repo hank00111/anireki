2b58858e3571060ebd88bc6f19afe86618a69714
hank00111 <42414986+hank00111@users.noreply.github.com>
2025-06-01 14:08:39 +0800

    v1.5.6.23 fix

--- src/pages/History.vue
+++ src/pages/History.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
-import Header from "../components/Header.vue";
 import Sidebar from "../components/Sidebar.vue";
 import WorksCard from "../components/WorksCard.vue";
 import { useAnimeWorks } from "../stores/animeWorks";
