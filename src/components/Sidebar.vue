34a284646a6e1a3b4370f2bb473c0b4bf0d5b89e
hank00111 <42414986+hank00111@users.noreply.github.com>
2025-08-04 10:58:30 +0800

    v1.5.6.33

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -39,13 +39,13 @@ const SwichDarkMode = () => {
                 </router-link>
             </li>
             <li>
-                <router-link to="/watchlater" class="router-link">
+                <!-- <router-link to="/watchlater" class="router-link">
                     <svg height="26" width="26" viewBox="0 -1020 960 960" fill="currentColor">
                         <path
                             d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z" />
                     </svg>
                     <span>稍後觀看</span>
-                </router-link>
+                </router-link> -->
             </li>
 
         </ul>
